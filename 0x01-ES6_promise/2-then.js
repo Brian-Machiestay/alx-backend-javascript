a3571ce26556a7760ebe7f0684ec1564fdf40db1
Brian-Machiestay <brianmachiestay@gmail.com>
2023-01-26 02:28:01 +0000

    attach three handlers

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,12 @@
 // thenable things
 
 export default function handleResponseFromAPI(promise) {
-  promise.then(console.log('Got a response from the API'))
+  promise.then(() => {
+    console.log('Got a response from the API');
+    return ({
+      status: 200,
+      body: 'success',
+    });
+  })
     .catch(new Error());
 }
