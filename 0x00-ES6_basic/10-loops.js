7ba27a9663b4d1bde6a7d30313abfef26d8f59f9
Brian-Machiestay <brianmachiestay@gmail.com>
2023-01-27 23:53:41 +0000

    rewrite code to use es6 for of loop

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
+  const arr = [];
   for (const idx of array) {
-    let value = array[idx];
-    array[idx] = appendString + value;
+    arr.push(appendString + idx);
   }
 
-  return array;
+  return arr;
 }
