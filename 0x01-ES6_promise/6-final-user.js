0a76f2cc9f9c06b229e61d41b31db2ea560396bd
Brian-Machiestay <brianmachiestay@gmail.com>
2023-01-28 04:47:20 +0000

    settle all promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,15 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => values);
+    .then((values) => {
+      const arr = [];
+      values.forEach((val) => {
+        const ob = {};
+        ob.status = val.status;
+        if (val.value) ob.value = val.value;
+        else ob.value = val.reason;
+        arr.push(ob);
+      })
+      return arr;
+    });
 }
