bf81c823c1fc928a8f953b768f7a9867992895e9
Brian-Machiestay <brianmachiestay@gmail.com>
2023-02-04 12:11:45 +0000

    check map usage implementation

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
 export default function getListStudentIds(stuObjs) {
   let arr = [];
-  if (stuObjs.constructor !== Array) return arr;
+  if (!stuObjs || stuObjs.constructor !== Array) return arr;
   arr = stuObjs.map((obj) => obj.id);
   return arr;
 }
