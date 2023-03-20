bf0ac5ef5497dc76201eed8e6f82ad3a278014cc
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-20 18:19:58 +0000

    check that output is correct

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,7 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
-  process.stdout.write(`your name is: ${data}`);
+  process.stdout.write(`Your name is: ${data}`);
+  process.exit();
 });
 process.on('exit', () => {
   process.stdout.write('This important software is now closing\n');
