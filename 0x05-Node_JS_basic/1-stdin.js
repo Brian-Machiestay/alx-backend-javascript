f9b7e3c9be676b93a6a7f61e7a8c1ddc34d4866a
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-20 18:21:46 +0000

    only display exit info in exit()

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,6 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
-  process.exit();
 });
 process.on('exit', () => {
   process.stdout.write('This important software is now closing\n');
