4df668396f18414290ac5e35bda4de84f970ac1b
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-20 17:52:26 +0000

    write explicitly to stdout

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,8 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
-  console.log(`your name is: ${data}`);
+  process.stdout.write(`your name is: ${data}`);
   process.exit();
 });
 process.on('exit', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
