67126a63821e7d625c1523065411d80d0bfe7f6e
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-22 05:48:11 +0000

    write utils

---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+const readDatabase = (path) => {
+  const ob = {};
+  return new Promise((resolve, reject) => {
+    if (err) reject(err);
+    else {
+      for (const line of data.split('\n')) {
+        if (line !== '') {
+          const fields = line.split(',');
+          if (fields[3] !== 'field') {
+            if (ob[fields[3]] === undefined) {
+              ob[fields[3]] = []
+            }
+            ob[fields[3]].push(fields[0]);
+          }
+        }
+      }
+      resolve(ob);
+    }
+  })
+}
