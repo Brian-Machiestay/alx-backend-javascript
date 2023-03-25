b18826da732479da6db9199964d10dc952876b33
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-25 05:32:14 +0000

    make sinon reconstruct references

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,8 +1,10 @@
 const Utils = require('./utils')
 const calculateNumber = Utils.calculateNumber;
 
-const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
-  const result = calculateNumber('SUM', totalAmount, totalShipping);
+const sendPaymentRequestToApi = (totalAmount, totalShipping, fn = null) => {
+  let result = '';
+  if (fn !== null) result = fn('SUM', totalAmount, totalShipping);
+  else result = calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total is: ${result}`);
   return result;
 }
