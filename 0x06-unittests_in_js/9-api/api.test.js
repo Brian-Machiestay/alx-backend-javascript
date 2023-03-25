db80d5005bf9ac654dc6122cf4d82bafa2806684
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-25 08:33:47 +0000

    check other endpoints

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,32 @@
+const request = require('request');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('query the server APIs', function () {
+  describe('test root endpoint', function () {
+    it('returns the root info', function () {
+      return request('http://localhost:7865', function (err, res, body) {
+        if (err) console.log(err);
+        expect(body).to.equal('Welcome to the payment system');
+        expect(res.statusCode).to.equal(200);
+      });
+    })
+  });
+
+  describe('test cart endpoint', function () {
+    it('correct matching cart endpoint returns 200', function () {
+      return request('http://localhost:7865/cart/12', function(err, res, body) {
+        if (err) console.log(err);
+        expect(body).to.equal('Payment methods for cart 12');
+        expect(res.statusCode).to.equal(200);
+      })
+    })
+
+    it('non-matching endpoint for cart returns 404', function () {
+      return request('http://localhost:7865/cart/hi', function(err, res, body) {
+        if (err) console.log(err);
+        expect(res.statusCode).to.equal(404);
+      })
+    })
+  })
+})
