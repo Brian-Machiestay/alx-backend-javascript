e74e0f41e73fd4cad7b4ab766af78cf416f3babe
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-25 07:39:31 +0000

    test api

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,12 @@
+const request = require('request');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('query the root of the server', function () {
+  it('returns the root info', function () {
+    return request('http://localhost:7865', function (err, res, body) {
+      if (err) console.log(err);
+      expect(body).to.equal('Welcome to the payment system');
+    });
+  });
+})
