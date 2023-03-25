ad7500c900dc7696cba1a732162125054b55fb72
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-25 07:49:32 +0000

    check the status

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -7,6 +7,7 @@ describe('query the root of the server', function () {
     return request('http://localhost:7865', function (err, res, body) {
       if (err) console.log(err);
       expect(body).to.equal('Welcome to the payment system');
+      expect(res.statusCode).to.equal(200);
     });
   });
 })
