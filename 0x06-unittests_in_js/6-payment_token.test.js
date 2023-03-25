506ca75da870e84f439c348fa1879237cd448723
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-25 07:04:06 +0000

    proper testing of asynchronous code without done

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,7 +1,11 @@
 const getPaymentTokenFromAPI = require('./6-payment_token');
+const chai = require('chai');
+const expect = chai.expect;
 
 describe('getPaymentTokenFromAPI', function () {
-  it('should be done when promise resolves', function(done) {
-    getPaymentTokenFromAPI(true).then(done())
+  it('should be done when promise resolves', async function() {
+    return getPaymentTokenFromAPI(true).then(function (data) {
+      expect(data).to.deep.equal({data: 'Successful response from the API'});
+    });
   });
 })
